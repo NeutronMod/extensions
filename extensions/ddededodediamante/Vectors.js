314a4a44e0e919da9f825a461af2809fc1fe0e9f
ddededodediamante <lukitadiamante5@gmail.com>
2026-03-14 13:23:31 -0300

    use Float32Array because snorfield told me to

--- extensions/ddededodediamante/Vectors.js
+++ extensions/ddededodediamante/Vectors.js
@@ -1,11 +1,3 @@
-// Name: Vectors
-// ID: ddeVectors
-// Description: Manipulate vectors with common math operations.
-// By: ddededodediamante <https://github.com/ddededodediamante/>
-// License: MPL-2.0
-
-// Version V.1.0.0
-
 (function (Scratch) {
   "use strict";
 
@@ -15,7 +7,6 @@
         id: "ddeVectors",
         name: "Vectors",
         color1: "#5BB998",
-
         blocks: [
           {
             opcode: "vector2D",
@@ -41,7 +32,7 @@
             text: Scratch.translate("vector from angle [A]"),
             blockType: Scratch.BlockType.ARRAY,
             arguments: {
-              A: { type: Scratch.ArgumentType.NUMBER, defaultValue: 0 },
+              A: { type: Scratch.ArgumentType.ANGLE, defaultValue: 0 },
             },
           },
           {
@@ -110,27 +101,54 @@
             },
           },
           {
-            opcode: "angle",
-            text: Scratch.translate("angle of [VEC]"),
+            opcode: "angle2D",
+            text: Scratch.translate("angle 2D of [VEC]"),
             blockType: Scratch.BlockType.REPORTER,
             arguments: {
               VEC: { type: Scratch.ArgumentType.ARRAY },
             },
           },
           {
-            opcode: "normalize",
-            text: Scratch.translate("normalize [VEC]"),
-            blockType: Scratch.BlockType.ARRAY,
+            opcode: "angle",
+            text: Scratch.translate("angle of [VEC] in axes [I] [J]"),
+            blockType: Scratch.BlockType.REPORTER,
             arguments: {
               VEC: { type: Scratch.ArgumentType.ARRAY },
+              I: { type: Scratch.ArgumentType.NUMBER, defaultValue: 0 },
+              J: { type: Scratch.ArgumentType.NUMBER, defaultValue: 1 },
             },
           },
           {
             opcode: "rotate2D",
-            text: Scratch.translate("rotate [VEC] by [A] degrees"),
+            text: Scratch.translate("rotate 2D [VEC] by [A] degrees"),
             blockType: Scratch.BlockType.ARRAY,
             arguments: {
               VEC: { type: Scratch.ArgumentType.ARRAY },
+              A: { type: Scratch.ArgumentType.ANGLE, defaultValue: 90 },
+            },
+          },
+          {
+            opcode: "vectorTransform",
+            text: Scratch.translate("[OP] [VEC]"),
+            blockType: Scratch.BlockType.ARRAY,
+            arguments: {
+              OP: {
+                type: Scratch.ArgumentType.STRING,
+                menu: "vectorTransform",
+              },
+              VEC: { type: Scratch.ArgumentType.ARRAY },
+            },
+          },
+          {
+            opcode: "rotate",
+            text: Scratch.translate(
+              "rotate [VEC] in axes [I] [J] by [A] degrees"
+            ),
+            blockType: Scratch.BlockType.ARRAY,
+            arguments: {
+              VEC: { type: Scratch.ArgumentType.ARRAY },
+              I: { type: Scratch.ArgumentType.NUMBER, defaultValue: 0 },
+              J: { type: Scratch.ArgumentType.NUMBER, defaultValue: 1 },
               A: { type: Scratch.ArgumentType.NUMBER, defaultValue: 90 },
             },
           },
@@ -153,67 +171,66 @@
             },
           },
           {
-            opcode: "dot",
-            text: Scratch.translate("dot product of [A] and [B]"),
+            opcode: "vectorOp",
+            text: Scratch.translate("[OP] [A] and [B]"),
             blockType: Scratch.BlockType.REPORTER,
             arguments: {
-              A: { type: Scratch.ArgumentType.ARRAY },
-              B: { type: Scratch.ArgumentType.ARRAY },
-            },
-          },
-          {
-            opcode: "distance",
-            text: Scratch.translate("distance between [A] and [B]"),
-            blockType: Scratch.BlockType.REPORTER,
-            arguments: {
-              A: { type: Scratch.ArgumentType.ARRAY },
-              B: { type: Scratch.ArgumentType.ARRAY },
-            },
-          },
-          {
-            opcode: "angleBetween",
-            text: Scratch.translate("angle between [A] and [B]"),
-            blockType: Scratch.BlockType.REPORTER,
-            arguments: {
+              OP: { type: Scratch.ArgumentType.STRING, menu: "vectorOp" },
               A: { type: Scratch.ArgumentType.ARRAY },
               B: { type: Scratch.ArgumentType.ARRAY },
             },
           },
         ],
+        menus: {
+          vectorOp: {
+            acceptReporters: false,
+            items: ["dot product of", "distance between", "angle between"],
+          },
+          vectorTransform: {
+            acceptReporters: false,
+            items: [
+              "normalize",
+              "absolute",
+              "negate",
+              "floor",
+              "ceil",
+              "round",
+            ],
+          },
+        },
       };
     }
 
     _map(a, func) {
-      return Scratch.Cast.toArray(a).map((x, i) =>
-        func(Scratch.Cast.toNumber(x), i)
-      );
+      const v = Scratch.Cast.toFloat32Array(a);
+      return Float32Array.from(v, func);
     }
 
     _map2(a, b, func) {
-      a = Scratch.Cast.toArray(a);
-      b = Scratch.Cast.toArray(b);
+      a = Scratch.Cast.toFloat32Array(a);
+      b = Scratch.Cast.toFloat32Array(b);
       const length = Math.max(a.length, b.length);
-      return Array.from({ length }, (_, i) =>
-        func(Scratch.Cast.toNumber(a[i] ?? 0), Scratch.Cast.toNumber(b[i] ?? 0))
+      return Float32Array.from({ length }, (_, i) =>
+        func(a[i] ?? 0, b[i] ?? 0)
       );
     }
 
     vector2D(args) {
-      return [Scratch.Cast.toNumber(args.X), Scratch.Cast.toNumber(args.Y)];
+      return new Float32Array([args.X, args.Y]);
     }
 
     vector3D(args) {
-      return [Scratch.Cast.toNumber(args.X), Scratch.Cast.toNumber(args.Y), Scratch.Cast.toNumber(args.Z)];
+      return new Float32Array([args.X, args.Y, args.Z]);
     }
 
     vectorFromAngle(args) {
       const rad = Scratch.Cast.toNumber(args.A) * (Math.PI / 180);
-      return [Math.cos(rad), Math.sin(rad)];
+      return new Float32Array([Math.cos(rad), Math.sin(rad)]);
     }
 
     dimensionsVector(args) {
       const n = Math.max(0, Math.floor(Scratch.Cast.toNumber(args.N)));
-      return Array(n).fill(Scratch.Cast.toNumber(args.V));
+      return new Float32Array(n).fill(Scratch.Cast.toNumber(args.V));
     }
 
     add(args) {
@@ -241,76 +258,117 @@
     }
 
     magnitude(args) {
-      const v = Scratch.Cast.toArray(args.VEC);
-      return Math.sqrt(
-        v.reduce((acc, x) => acc + Math.pow(Scratch.Cast.toNumber(x), 2), 0)
-      );
+      const v = Scratch.Cast.toFloat32Array(args.VEC);
+      let sum = 0;
+      for (let i = 0; i < v.length; i++) sum += v[i] * v[i];
+      return Math.sqrt(sum);
+    }
+
+    angle2D(args) {
+      const v = Scratch.Cast.toFloat32Array(args.VEC);
+      return Math.atan2(v[1] ?? 0, v[0] ?? 0) * (180 / Math.PI);
     }
 
     angle(args) {
-      const v = Scratch.Cast.toArray(args.VEC);
-      const x = Scratch.Cast.toNumber(v[0] ?? 0);
-      const y = Scratch.Cast.toNumber(v[1] ?? 0);
-      return Math.atan2(y, x) * (180 / Math.PI);
-    }
-
-    normalize(args) {
-      const mag = this.magnitude(args);
-      if (mag === 0) return this._map(args.VEC, () => 0);
-      return this._map(args.VEC, (x) => x / mag);
+      const v = Scratch.Cast.toFloat32Array(args.VEC);
+      const i = Math.floor(Scratch.Cast.toNumber(args.I));
+      const j = Math.floor(Scratch.Cast.toNumber(args.J));
+      return Math.atan2(v[j] ?? 0, v[i] ?? 0) * (180 / Math.PI);
     }
 
     rotate2D(args) {
-      const v = Scratch.Cast.toArray(args.VEC);
+      const v = Scratch.Cast.toFloat32Array(args.VEC);
       const rad = Scratch.Cast.toNumber(args.A) * (Math.PI / 180);
-      const x = Scratch.Cast.toNumber(v[0] ?? 0);
-      const y = Scratch.Cast.toNumber(v[1] ?? 0);
       const cos = Math.cos(rad);
       const sin = Math.sin(rad);
-      const result = [x * cos - y * sin, x * sin + y * cos];
-      for (let i = 2; i < v.length; i++)
-        result.push(Scratch.Cast.toNumber(v[i]));
+      const result = new Float32Array(v);
+      result[0] = v[0] * cos - (v[1] ?? 0) * sin;
+      result[1] = v[0] * sin + (v[1] ?? 0) * cos;
       return result;
+    }
+
+    rotate(args) {
+      const v = Scratch.Cast.toFloat32Array(args.VEC);
+      const i = Math.floor(Scratch.Cast.toNumber(args.I));
+      const j = Math.floor(Scratch.Cast.toNumber(args.J));
+      const rad = Scratch.Cast.toNumber(args.A) * (Math.PI / 180);
+      const cos = Math.cos(rad);
+      const sin = Math.sin(rad);
+      const xi = v[i] ?? 0;
+      const xj = v[j] ?? 0;
+      const result = new Float32Array(v);
+      result[i] = xi * cos - xj * sin;
+      result[j] = xi * sin + xj * cos;
+      return result;
+    }
+
+    vectorTransform(args) {
+      switch (args.OP) {
+        case "normalize": {
+          const mag = this.magnitude({ VEC: args.VEC });
+          if (mag === 0) return this._map(args.VEC, () => 0);
+          return this._map(args.VEC, (x) => x / mag);
+        }
+        case "absolute":
+          return this._map(args.VEC, Math.abs);
+        case "negate":
+          return this._map(args.VEC, (x) => -x);
+        case "floor":
+          return this._map(args.VEC, Math.floor);
+        case "ceil":
+          return this._map(args.VEC, Math.ceil);
+        case "round":
+          return this._map(args.VEC, Math.round);
+      }
     }
 
     clampMagnitude(args) {
       const max = Scratch.Cast.toNumber(args.MAX);
       const mag = this.magnitude(args);
       if (mag === 0 || mag <= max)
-        return Scratch.Cast.toArray(args.VEC).map((x) =>
-          Scratch.Cast.toNumber(x)
-        );
+        return new Float32Array(Scratch.Cast.toFloat32Array(args.VEC));
       return this._map(args.VEC, (x) => (x / mag) * max);
     }
 
     swizzle(args) {
-      const v = Scratch.Cast.toArray(args.VEC);
-      return Scratch.Cast.toArray(args.INDICES).map(i =>
-        Scratch.Cast.toNumber(v[Scratch.Cast.toNumber(i)] ?? 0)
-      );
+      const v = Scratch.Cast.toFloat32Array(args.VEC);
+      const indices = Scratch.Cast.toFloat32Array(args.INDICES);
+      return Float32Array.from(indices, (i) => v[i] ?? 0);
     }
 
-    dot(args) {
-      return this._map2(args.A, args.B, (x, y) => x * y).reduce(
-        (acc, x) => acc + x,
-        0
-      );
-    }
-
-    distance(args) {
-      const diff = this._map2(args.A, args.B, (x, y) => x - y);
-      return Math.sqrt(diff.reduce((acc, x) => acc + x * x, 0));
-    }
-
-    angleBetween(args) {
-      const magA = this.magnitude({ VEC: args.A });
-      const magB = this.magnitude({ VEC: args.B });
-      if (magA === 0 || magB === 0) return 0;
-      const cosAngle = Math.max(
-        -1,
-        Math.min(1, this.dot(args) / (magA * magB))
-      );
-      return Math.acos(cosAngle) * (180 / Math.PI);
+    vectorOp(args) {
+      switch (args.OP) {
+        case "dot product of": {
+          const a = Scratch.Cast.toFloat32Array(args.A);
+          const b = Scratch.Cast.toFloat32Array(args.B);
+          let sum = 0;
+          for (let i = 0; i < Math.max(a.length, b.length); i++)
+            sum += (a[i] ?? 0) * (b[i] ?? 0);
+          return sum;
+        }
+        case "distance between": {
+          const diff = this._map2(args.A, args.B, (x, y) => x - y);
+          let sum = 0;
+          for (let i = 0; i < diff.length; i++) sum += diff[i] * diff[i];
+          return Math.sqrt(sum);
+        }
+        case "angle between": {
+          const magA = this.magnitude({ VEC: args.A });
+          const magB = this.magnitude({ VEC: args.B });
+          if (magA === 0 || magB === 0) return 0;
+          const a = Scratch.Cast.toFloat32Array(args.A);
+          const b = Scratch.Cast.toFloat32Array(args.B);
+          let dot = 0;
+          for (let i = 0; i < Math.max(a.length, b.length); i++)
+            dot += (a[i] ?? 0) * (b[i] ?? 0);
+          return (
+            Math.acos(Math.max(-1, Math.min(1, dot / (magA * magB)))) *
+            (180 / Math.PI)
+          );
+        }
+        default:
+          return 0;
+      }
     }
   }
 
