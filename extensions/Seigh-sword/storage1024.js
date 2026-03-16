3105b15c8168dd19048f8bf54ce508c80c053eeb
Seigh sword <zack.yt.7085@gmail.com>
2026-03-16 11:45:23 +0530

    Update storage1024.js

--- extensions/Seigh-sword/storage1024.js
+++ extensions/Seigh-sword/storage1024.js
@@ -1,1 +1,8 @@
+// Name: Storage1024
+// ID: s1024
+// Description: Store files, and global variables using your token! and id!
+// By: ddededodediamante <https://github.com/Seigh-sword/>
+// License: MPL-2.0
+
+// Version V.1.0.0
 
